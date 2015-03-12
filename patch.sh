a78e5f8ef5cc835cdd1b8b7c649c3210fd513994
richelbilderbeek <richel@richelbilderbeek.nl>
2015-03-12 20:47:16 +0100

    Fixed bug in patch

--- patch.sh
+++ patch.sh
@@ -1,8 +1,19 @@
-svn checkout http://svn.code.sf.net/p/openastromenace/code/ .
+
+
+if [ d AstroMenaceSource ]
+then
+  svn checkout http://svn.code.sf.net/p/openastromenace/code/ .
+fi
+
+# Might install more than the minimum, feel free to improve this by submitting 
+# an Issue on the GitHub :-)
+sudo apt-get install libopenal1 libalut0 libogg0 libvorbis*
+
 cp AstroMenace.patch ../
 cd ..
 patch -p0 -i AstroMenace.patch
 rm AstroMenace.patch
+cd AstroMenaceCheat
 cmake ./
 make
 ./AstroMenace --pack --rawdata=./RAW_VFS_DATA
