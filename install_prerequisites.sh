a0d0835203e9bee384b998fc27a3b892dedacb8f
richelbilderbeek <richel@richelbilderbeek.nl>
2023-09-22 14:28:18 +0200

    Add CI

---
+++ install_prerequisites.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Might install more than the minimum, feel free to improve this by submitting 
+# an Issue on the GitHub :-)
+sudo apt-get install libopenal1 libalut0 libogg0 libvorbis* libsdl2-dev libopenal-dev libalut-dev
--- patch.sh
+++ patch.sh
@@ -1,33 +1,31 @@
-
+#!/bin/bash
 
 if [ ! -d AstroMenaceSource ]
 then
-  svn checkout http://svn.code.sf.net/p/openastromenace/code/ .
+  git clone https://github.com/viewizard/astromenace
+
 fi
 
-# Might install more than the minimum, feel free to improve this by submitting 
-# an Issue on the GitHub :-)
-sudo apt-get install libopenal1 libalut0 libogg0 libvorbis*
+# cp AstroMenace.patch ../
+# cd ..
+# 
+# if [ ! -e AstroMenace.patch ]
+# then
+#   echo "ERROR: AstroMenace.patch not found in parent folder of AstroMenaceCheat"
+#   exit
+# fi
 
-cp AstroMenace.patch ../
-cd ..
+# patch -p0 -i AstroMenace.patch
+# rm AstroMenace.patch
 
-if [ ! -e AstroMenace.patch ]
+if [ ! -d astromenace ]
 then
-  echo "ERROR: AstroMenace.patch not found in parent folder of AstroMenaceCheat"
+  echo "ERROR: 'astromenace' folder not found"
   exit
 fi
 
-patch -p0 -i AstroMenace.patch
-rm AstroMenace.patch
+cd astromenace || exit 42
 
-if [ ! -d AstroMenaceCheat ]
-then
-  echo "ERROR: AstroMenaceCheat folder not found in parent folder of AstroMenaceCheat"
-  exit
-fi
-
-cd AstroMenaceCheat
 cmake ./
 make
 ./AstroMenace --pack --rawdata=./RAW_VFS_DATA
